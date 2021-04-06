758479faaf853a97fca18f804f21d2c9c50ecca6
TranHuuPhu1998 <tranhuuphu877@gmail.com>
2021-04-06 22:42:04 +0700

    edit

--- app/app/Http/Controllers/ResetPasswordController.php
+++ app/app/Http/Controllers/ResetPasswordController.php
@@ -26,44 +26,8 @@ class ResetPasswordController extends Controller
      */
 
     
-    // public function sendMail(Request $request)
-    // {
-    //     $user = User::where('email', $request->email)->firstOrFail();
-
-    //     $passwordReset = PasswordReset::updateOrCreate([
-    //         'email' => $user->email,
-    //     ], [
-    //         'token' => Str::random(60),
-    //     ]);
-
-    //     if ($passwordReset) {
-    //         $user->notify(new ResetPasswordRequest($passwordReset->token));
-    //     }
-  
-    //     return response()->json([
-    //         'message' => 'We have e-mailed your password reset link!'
-    //     ]);
-        
-    // }
-    
     public function reset(ResetRequest $request)
     {
-        // $passwordReset = PasswordReset::where('token', $token)->firstOrFail();
-        // if (Carbon::parse($passwordReset->updated_at)->addMinutes(720)->isPast()) {
-        //     $passwordReset->delete();
-
-        //     return response()->json([
-        //         'message' => 'This password reset token is invalid.',
-        //     ], 422);
-        // }
-        // $user = User::where('email', $passwordReset->email)->firstOrFail();
-        // $updatePasswordUser = $user->update($request->only('password'));
-        // $passwordReset->delete();
-
-        // return response()->json([
-        //     'success' => $updatePasswordUser,
-        // ]);
-
         $token = $request->input('token');
 
         if(!$passwordReset = DB::table('password_resets')->where('token',$token)->first()){
@@ -74,7 +38,7 @@ class ResetPasswordController extends Controller
 
         if(!$user = User::where('email' , $passwordReset->email)->first()){
             return response([
-                'message' => 'user does not exist !'
+                'message' => 'user does not exist!'
             ],404);
         }
 
